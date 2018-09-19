dc9d2362f9b78468a59329791e4f416bd4373b36
Tim Dunn <dunntj@clarkson.edu>
2018-09-18 21:21:59 -0400

    Segmenting script now callable as function
     - supply video filename, all segments generated in "segments" subfolder

---
+++ main.py
@@ -0,0 +1,6 @@
+import segment
+
+# slice up entire video into a segment for each swipe
+segment.generate_segments("test_data/hard_paper.mov", True);
+
+
--- segment.py
+++ segment.py
@@ -20,93 +20,109 @@ Y0, Y1 = 224, 444
 X0, X1 = 168, 376
 
 
-# check input arguments, set filepaths
-if len(sys.argv) != 2:
-	print("Usage: python segment.py <filename>")
-	sys.exit()
-filename, filetype = os.path.splitext(sys.argv[1])
-folder, filename = os.path.split(filename)
-if len(folder) > 0:
-	folder = folder+"/"
-if not os.path.isfile(folder+filename+filetype):
-	print("ERROR: File", folder+filename+filetype, "does not exist.")
-	sys.exit()
-if not os.path.exists(folder+"segments"):
-	os.makedirs(folder+"segments")
+def generate_segments(full_filename, summary=False):
 
-# set flags
-last_new_segment = 0	# frame at which segment began
-was_low = False			# if hand left image since segment began
-low_count = 0			# consecutive frames for which hand was gone
-segment = 0				# segment id number
+	filename, filetype = os.path.splitext(full_filename)
+	folder, filename = os.path.split(filename)
+	if len(folder) > 0:
+		folder = folder+"/"
+	if not os.path.isfile(folder+filename+filetype):
+		print("ERROR: File", folder+filename+filetype, "does not exist.")
+		sys.exit()
+	if not os.path.exists(folder+"segments"):
+		os.makedirs(folder+"segments")
 
-# initialize summary/segment writers, set background as first frame
-reader = imageio.get_reader(folder+filename+filetype, 'ffmpeg')
-fps = reader.get_meta_data()['fps']
-nframes = reader.get_meta_data()['nframes']
-segment_writer = imageio.get_writer(folder+"segments/"+filename+str(segment)+filetype, 
-		'ffmpeg', fps=fps, macro_block_size=None)
-segment_writer.close()
-summary_writer = imageio.get_writer(folder+filename+"_summary"+filetype, 
-		'ffmpeg', fps=fps*2)
-background = np.array(reader.get_data(0)).astype(int)[:,:,0]
+	# set flags
+	last_new_segment = 0	# frame at which segment began
+	was_low = False			# if hand left image since segment began
+	low_count = 0			# consecutive frames for which hand was gone
+	segment = 0				# segment id number
 
-# process video and segment
-for i, image in enumerate(reader):
+	# initialize summary/segment writers, set background as first frame
+	reader = imageio.get_reader(folder+filename+filetype, 'ffmpeg')
+	fps = reader.get_meta_data()['fps']
+	nframes = reader.get_meta_data()['nframes']
+	segment_writer = imageio.get_writer(
+			folder+"segments/"+filename+str(segment)+filetype, 
+			'ffmpeg', fps=fps, macro_block_size=None)
+	segment_writer.close()
+	if summary:
+		summary_writer = imageio.get_writer(folder+filename+"_summary"+filetype, 
+			'ffmpeg', fps=fps*2)
+	background = np.array(reader.get_data(0)).astype(int)[:,:,0]
 
-	# background subtract, threshold at zero
-	image = np.array(image).astype(int)[:,:,0]
-	image = np.maximum(image - background, np.zeros(image.shape))
+	# process video and segment
+	for i, image in enumerate(reader):
 
-	# check if at least 10 bottom pixels belong to a hand
-	if(np.sum(image[Y1:Y1+1, X0:X1] > HAND_CUTOFF) > 3):
+		# background subtract, threshold at zero
+		image = np.array(image).astype(int)[:,:,0]
+		image = np.maximum(image - background, np.zeros(image.shape))
 
-		# if hand just entered image and segment was long enough, start new segment
-		if(i - last_new_segment > MIN_SEGMENT_LENGTH and was_low):
+		# check if at least 10 bottom pixels belong to a hand
+		if(np.sum(image[Y1:Y1+1, X0:X1] > HAND_CUTOFF) > 3):
+
+			# if hand just entered image and segment was long enough, start new segment
+			if(i - last_new_segment > MIN_SEGMENT_LENGTH and was_low):
+				if not segment_writer.closed:
+					segment_writer.close()
+				segment += 1
+				segment_writer = imageio.get_writer(
+						folder+"segments/"+filename+str(segment)+filetype, 
+						'ffmpeg', fps=fps, macro_block_size=None)
+				last_new_segment = i
+				was_low = False
+				low_count = 0
+
+		else: # hand isn't in image, after 1/10 second decide it has left
+			low_count += 1
+			if low_count >= 3:
+				was_low = True
+			
+		# segment has reached maximum length, end it
+		if i - last_new_segment > MAX_SEGMENT_LENGTH:
 			if not segment_writer.closed:
 				segment_writer.close()
-			segment += 1
-			segment_writer = imageio.get_writer(
-					folder+"segments/"+filename+str(segment)+filetype, 
-					'ffmpeg', fps=fps, macro_block_size=None)
-			last_new_segment = i
-			was_low = False
-			low_count = 0
 
-	else: # hand isn't in image, after 1/10 second decide it has left
-		low_count += 1
-		if low_count >= 3:
-			was_low = True
-		
-	# segment has reached maximum length, end it
-	if i - last_new_segment > MAX_SEGMENT_LENGTH:
+		# add border around bounding area which is captured 
+		image[Y0-1,X0:X1] = WHITE*np.ones(X1-X0)
+		image[Y1,X0:X1] = WHITE*np.ones(X1-X0)
+		image[Y0:Y1,X0-1] = WHITE*np.ones(Y1-Y0)
+		image[Y0:Y1,X1] = WHITE*np.ones(Y1-Y0)
+
+		# record with segment/summary writers
 		if not segment_writer.closed:
-			segment_writer.close()
+			segment_writer.append_data(image[Y0:Y1,X0:X1].astype('uint8'))
+			if summary:
+				summary_writer.append_data(image.astype('uint8'))
 
-	# add border around bounding area which is captured 
-	image[Y0-1,X0:X1] = WHITE*np.ones(X1-X0)
-	image[Y1,X0:X1] = WHITE*np.ones(X1-X0)
-	image[Y0:Y1,X0-1] = WHITE*np.ones(Y1-Y0)
-	image[Y0:Y1,X1] = WHITE*np.ones(Y1-Y0)
+		else: # add lines to indicate not recording, add to summary writer
+			for x in range(X0, X1, 10):
+				image[Y0:Y1,x] = WHITE*np.ones(Y1-Y0)
+			if summary:
+				summary_writer.append_data(image.astype('uint8'))
 
-	# record with segment/summary writers
-	if not segment_writer.closed:
-		segment_writer.append_data(image[Y0:Y1,X0:X1].astype('uint8'))
-		summary_writer.append_data(image.astype('uint8'))
-
-	else: # add lines to indicate not recording, add to summary writer
-		for x in range(X0, X1, 10):
-			image[Y0:Y1,x] = WHITE*np.ones(Y1-Y0)
-		summary_writer.append_data(image.astype('uint8'))
-
-	# display progress processing video
-	if i % 100 == 0:
-		percent = (i / nframes)
-		bars = percent*40
-		sys.stdout.write("\rProgress: [{0}{1}] {2}%".format("|"*int(bars), " "*int(40-bars), int(percent*100)))
-		sys.stdout.flush()
+		# display progress processing video
+		if i % 100 == 0:
+			percent = (i / nframes)
+			bars = percent*40
+			sys.stdout.write("\rSegmenting: [{0}{1}] {2}%   ".format("|"*int(bars), \
+				" "*int(40-bars), int(percent*100)))
+			sys.stdout.flush()
 
 # close writers
-print("")
-segment_writer.close()
-summary_writer.close()
+	print("")
+	segment_writer.close()
+	if summary:
+		summary_writer.close()
+
+
+
+if __name__ == "__main__":
+
+	# check input arguments, set filepaths
+	if len(sys.argv) != 2:
+		print("Usage: python segment.py <filename>")
+		sys.exit()
+
+	# call function to generate segments
+	generate_segments(sys.argv[1], True);
